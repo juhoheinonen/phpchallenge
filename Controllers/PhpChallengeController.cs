bccaf22ce9cb0f87e7856d6dbc5cb5475f8fae89
Juho Heinonen <juho.heinonen@innofactor.com>
2019-12-14 17:16:49 +0200

    Initial commit.

---
+++ Controllers/PhpChallengeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using webapi.Services;
+
+namespace webapi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PhpChallengeController : ControllerBase
+    {
+        private readonly ILogger<PhpChallengeController> _logger;
+
+        private PhpChallengeService _phpChallengeService;
+
+        private PhpChallengeService PhpChallengeService
+        {
+            get
+            {
+                if (_phpChallengeService == null)
+                {
+                    _phpChallengeService = new PhpChallengeService();
+                }
+                return _phpChallengeService;
+            }
+        }
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        public PhpChallengeController(ILogger<PhpChallengeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "test";
+        }
+
+        [HttpPost("sanaruudukko")]
+        public string Sanaruudukko([FromQuery] string rivit, [FromQuery] string sanat)
+        {
+            int result = PhpChallengeService.Sanaruudukko(rivit, sanat);
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Services/PhpChallengeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace webapi.Services
+{
+    public class PhpChallengeService
+    {
+        public PhpChallengeService()
+        {
+
+        }
+
+        internal int Sanaruudukko(string rivit, string sanat)
+        {
+            return 0;
+        }
+    }
+}
